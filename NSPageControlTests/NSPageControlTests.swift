4878090bdc01297a1d23c2b567e31804bc85ce69
Kohei Tabata <nerd0geek1@gmail.com>
2016-03-25 20:51:34 +0900

    add TestCase for valid/invalid CurrentPage value

--- NSPageControlTests/NSPageControlTests.swift
+++ NSPageControlTests/NSPageControlTests.swift
@@ -6,30 +6,37 @@
 //  Copyright © 2016年 Kohei Tabata. All rights reserved.
 //
 
+import NSPageControl
 import XCTest
 
 class NSPageControlTests: XCTestCase {
-    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    func testToSetValidCurrentPage() {
+        let pageControl: NSPageControl = NSPageControl(frame: NSRect.init(x: 0, y: 0, width: 100, height: 10))
+        pageControl.numberOfPages = 3
+        pageControl.drawRect(pageControl.frame)
+        XCTAssertTrue(pageControl.currentPage == 0)
+        pageControl.currentPage = 1
+        XCTAssertTrue(pageControl.currentPage == 1)
+        pageControl.currentPage = 2
+        XCTAssertTrue(pageControl.currentPage == 2)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
+
+    func testToSetInvalidCurrentPage() {
+        let pageControl: NSPageControl = NSPageControl(frame: NSRect.init(x: 0, y: 0, width: 100, height: 10))
+        pageControl.numberOfPages = 3
+        pageControl.drawRect(pageControl.frame)
+        XCTAssertTrue(pageControl.currentPage == 0)
+        pageControl.currentPage = 3
+        XCTAssertTrue(pageControl.currentPage == 2)
+        pageControl.currentPage = -1
+        XCTAssertTrue(pageControl.currentPage == 0)
     }
-    
 }
