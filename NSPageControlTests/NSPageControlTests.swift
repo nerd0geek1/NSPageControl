784b693bf29fd8a6bb1e0c911b50b762bdbff1b1
Kohei Tabata <nerd0geek1@gmail.com>
2016-03-25 20:28:06 +0900

    add Unit Testing Target

---
+++ NSPageControlTests/NSPageControlTests.swift
@@ -0,0 +1,35 @@
+//
+//  NSPageControlTests.swift
+//  NSPageControlTests
+//
+//  Created by Kohei Tabata on 2016/03/25.
+//  Copyright © 2016年 Kohei Tabata. All rights reserved.
+//
+
+import XCTest
+
+class NSPageControlTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
