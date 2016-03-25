ff088a21b8a63d27e23932bae178a1812c8d075e
Kohei Tabata <nerd0geek1@gmail.com>
2016-03-25 19:11:51 +0900

    add NSPageControl.framework on Example project

---
+++ Example/Example/NSPageControl.framework/Versions/A/Headers/NSPageControl.h
@@ -0,0 +1,19 @@
+//
+//  NSPageControl.h
+//  NSPageControl
+//
+//  Created by Kohei Tabata on 2016/03/24.
+//  Copyright © 2016年 Kohei Tabata. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NSPageControl.
+FOUNDATION_EXPORT double NSPageControlVersionNumber;
+
+//! Project version string for NSPageControl.
+FOUNDATION_EXPORT const unsigned char NSPageControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSPageControl/PublicHeader.h>
+
+
