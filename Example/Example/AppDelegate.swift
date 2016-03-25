b589af8f372ad895858b172510bcf60cce8256ca
Kohei Tabata <nerd0geek1@gmail.com>
2016-03-25 19:04:57 +0900

    add Example project

---
+++ Example/Example/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Example
+//
+//  Created by Kohei Tabata on 2016/03/25.
+//  Copyright © 2016年 Kohei Tabata. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
